b4f956402b1d99a98684354a3c5cefa03bd912ee
Nort <4knort@gmail.com>
2017-01-05 21:43:09 +0200

    changed output webpack

--- webpack.config.js
+++ webpack.config.js
@@ -124,7 +124,7 @@ module.exports = {
     ],
 
   output: {
-    path: path.join(__dirname, 'public'),
+    path: path.join(__dirname, 'public/'),
     filename: 'bundle.js',
     publicPath: pagesBuild ? '/new_sort_redux' : '/',
   },
