6222bb7a3a33509487dd3cf8be4dab7fd380314b
Nort <4knort@gmail.com>
2017-01-06 20:53:46 +0200

    changed header location

--- src/components/AppContainer/AppContainer.jsx
+++ src/components/AppContainer/AppContainer.jsx
@@ -1,7 +1,12 @@
 import React, { PropTypes } from 'react';
+import Header from '../Header.jsx';
 
 const AppContainer = ({ children }) => (
-  <div className="app">{children}</div>
+  <div>
+    <Header />
+    <div className="app">{children}</div>
+  </div>
+  
 );
 
 AppContainer.propTypes = {
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Avatar from './Avatars/Avatar';
+
+const Header = () => {
+  return (
+    <div className="container-fluid rc-intro">
+      <div className="container">
+        <Avatar image={'react'} />
+        <h1 className="rc-title">React Challenge</h1>
+        <p className="rc-description">Поиск и сортировка данных</p>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
